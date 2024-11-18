ab8be3555626f85ba182cc0c6009526c4f715552
nicoly2410 <128054872+nicoly2410@users.noreply.github.com>
2024-11-18 08:45:39 -0300

    Add files via upload

---
+++ Av02/Cliente.java
@@ -0,0 +1,19 @@
+
+public class Cliente {
+  private String nome;
+  private String cpf;
+  private String endereco;
+  private String telefone;
+
+  public Cliente(String nome, String cpf, String endereco, String telefone) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.endereco = endereco;
+    this.telefone = telefone;
+  }
+
+  @Override
+  public String toString() {
+    return "Nome: " + nome + ", CPF: " + cpf + ", Endereco: " + endereco + ", Telefone: " + telefone;
+  }
+}
---
+++ Av02/Main.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Cadastro cadastro = new Cadastro();
+
+    while (true) {
+      System.out.println("1. Cadastrar cliente");
+      System.out.println("2. Listar cliente");
+      System.out.println("3. Sair");
+      System.out.print("Escolha uma opção: ");
+
+      int opcao = scanner.nextInt();
+      scanner.nextLine();
+      System.out.println("");
+
+      switch (opcao) {
+        case 1:
+          System.out.println("Digite o nome do cliente: ");
+          String nome = scanner.nextLine();
+          System.out.println("Digite o CPF do cliente: ");
+          String cpf = scanner.nextLine();
+          System.out.println("Digite o endereco do cliente: ");
+          String endereco = scanner.nextLine();
+          System.out.println("Digite o telefone do cliente: ");
+          String telefone = scanner.nextLine();
+          System.out.println("");
+          // Criando cadastro do novo cliente
+          Cliente cliente = new Cliente(nome, cpf, endereco, telefone);
+          cadastro.cadastrarCliente(cliente);
+          break;
+        case 2:
+          // System.out.println("Lista de cadastros: ");
+          // listando todos os clientes
+          cadastro.listarClientes();
+          System.out.println("");
+          break;
+
+        case 3:
+          System.out.println("Saindo...");
+          scanner.close();
+          return;
+
+        default:
+          System.out.println("Opção inválida ou vazia... Tente novamente!");
+          System.out.println("");
+      }
+    }
+  }
+}
